e5b09ce08adbb002db997c8f65ac77f7a02a3a0f
John Drago <john.drago@healthfidelity.com>
2016-11-15 12:16:06 -0800

    Fix some permission issues

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,23 @@ FROM ubuntu:15.10
 
 RUN \
   apt-get update \
+  && apt-get install -y python-pip python-dev sudo \
+  && pip install credstash \
   && apt-get install --fix-missing -y \
     rabbitmq-server
+
+# Make sure the 'ubuntu' user has sudo privileges:
+RUN \
+  useradd -d /home/ubuntu -m -s /bin/bash ubuntu && \
+  echo "ubuntu:changeme" | chpasswd && \
+  echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
+  sed -i s#/home/ubuntu:/bin/false#/home/ubuntu:/bin/bash# /etc/passwd
 
 COPY docker-entrypoint.sh /
 
 EXPOSE 4369 5671 5672 15671 15672 25672
 CMD ["rabbitmq-server"]
+
+#USER ubuntu
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,41 +6,57 @@ export RABBITMQ_DEFAULT_USER
 export RABBITMQ_DEFAULT_PASS
 export RABBITMQ_DEFAULT_VHOST=${RABBITMQ_DEFAULT_VHOST:-"/"}
 
-service rabbitmq-server start
+vars="
+  RABBITMQ_DEFAULT_USER
+  RABBITMQ_DEFAULT_PASS
+"
+
+for var in $vars
+do
+  # If the variable is already defined, then use it; otherwise, pull from AWS.
+  eval val=\$$var
+  if [ -z "$val" ]; then
+    eval "export $var=$(credstash get $var)"
+  else
+    export $var
+  fi
+done
+
+sudo service rabbitmq-server start
 sleep 5
 echo 'enabling...'
-rabbitmq-plugins enable rabbitmq_management
+sudo rabbitmq-plugins enable rabbitmq_management
 
 echo 'list users?'
-rabbitmqctl list_users
+sudo rabbitmqctl list_users
 
-if rabbitmqctl list_users | grep $RABBITMQ_DEFAULT_USER; then
+if sudo rabbitmqctl list_users | grep $RABBITMQ_DEFAULT_USER; then
   echo "user '$RABBITMQ_DEFAULT_USER' already exists"
 else
   echo 'adding user.....'
   set
-  rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS || echo 'weird...'
+  sudo rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS || echo 'weird...'
 fi
 
 echo 'list users (2)'
-if rabbitmqctl list_users | grep $RABBITMQ_DEFAULT_USER | grep -F "[administrator]"; then
+if sudo rabbitmqctl list_users | grep $RABBITMQ_DEFAULT_USER | grep -F "[administrator]"; then
   echo "user '$RABBITMQ_DEFAULT_USER' already tagged as 'administrator'"
 else
   echo 'set user tags'
-  rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
+  sudo rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
 fi
 
-rabbitmqctl add_vhost $RABBITMQ_DEFAULT_VHOST || echo "vhost '$RABBITMQ_DEFAULT_VHOST' already exists"
+sudo rabbitmqctl add_vhost $RABBITMQ_DEFAULT_VHOST || echo "vhost '$RABBITMQ_DEFAULT_VHOST' already exists"
 
 echo 'list permissions?'
-if rabbitmqctl list_permissions -p $RABBITMQ_DEFAULT_VHOST | grep $RABBITMQ_DEFAULT_USER | grep "$RABBITMQ_DEFAULT_USER\\.\\*\\s+\\.\\*\\s+\\.\\*\$/"; then
+if sudo rabbitmqctl list_permissions -p $RABBITMQ_DEFAULT_VHOST | grep $RABBITMQ_DEFAULT_USER | grep "$RABBITMQ_DEFAULT_USER\\.\\*\\s+\\.\\*\\s+\\.\\*\$/"; then
   echo "user '$RABBITMQ_DEFAULT_USER' already has permissions '.* .* .*'"
 else
   echo 'setting permissions'
-  rabbitmqctl set_permissions -p $RABBITMQ_DEFAULT_VHOST $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+  sudo rabbitmqctl set_permissions -p $RABBITMQ_DEFAULT_VHOST $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
 fi
 
-service rabbitmq-server stop
+sudo service rabbitmq-server stop
 
 echo 'finally'
 exec "$@"
